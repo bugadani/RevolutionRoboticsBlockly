00a0c99e0dc9fd1294594436aa6d03c072876bfc
Mate Papp <mate.papp@supercharge.io>
2019-07-05 16:50:36 +0200

    Remove number from inputs name

--- blockly/blocks/custom_blocks.js
+++ blockly/blocks/custom_blocks.js
@@ -455,8 +455,7 @@ Blockly.Blocks['block_ultrasonic_sensor'] = {
             new Blockly.FieldImage(CUSTOM_IMAGES.ULTRASONIC, 15, 15, '*'),
             'ULTRASONIC_IMAGE'
         )
-        .appendField(new Blockly.FieldTextInput('ultrasonic1'), 'NAME_INPUT')
-        .appendField('distance');
+        .appendField(new Blockly.FieldTextInput('distance'), 'NAME_INPUT');
     this.setOutput(true, 'Number');
     this.setStyle('colour_blocks');
     this.setTooltip('');
@@ -469,11 +468,11 @@ Blockly.Blocks['block_bumper'] = {
   init: function() {
     this.appendDummyInput()
         .appendField(new Blockly.FieldImage(CUSTOM_IMAGES.BUMPER, 15, 15, '*'), 'BUMPER_IMAGE')
-        .appendField(new Blockly.FieldTextInput('bumper1'), 'NAME_INPUT')
+        .appendField(new Blockly.FieldTextInput('bumper'), 'NAME_INPUT')
         .appendField('pressed');
 
     this.setOutput(true, 'Boolean');
-    this.setStyle('colour_blocks');
+    this.setColour('#0264ff');
     this.setTooltip('');
     this.setHelpUrl('');
   }
