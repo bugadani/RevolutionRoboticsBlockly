416552acfaeedc56b7dc214c1330a4980dd8c804
Buga Dániel <daniel.buga@revolutionrobotics.org>
2020-02-03 14:39:46 +0100

    Fix syntax error in generated python code
    Fixes issue #15

--- blockly/generators/python/custom_motor.js
+++ blockly/generators/python/custom_motor.js
@@ -218,5 +218,5 @@ Blockly.Python['block_stop_all_motors'] = function (block) {
     var loop_var = Blockly.Python.variableDB_.getDistinctName('motor', Blockly.Variables.NAME_TYPE);
 
     return `for ${loop_var} in robot.motors:\n` +
-            Blockly.Python.INDENT + `${loop_var}.stop(action='${dropdown_action})\n`;
+            Blockly.Python.INDENT + `${loop_var}.stop(action=${dropdown_action})\n`;
 };
